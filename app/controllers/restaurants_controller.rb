af839e1aec9cca0874c3987392171538589b785d
Antoine de Broqueville <antoinedebroqueville@gmail.com>
2020-03-11 16:46:04 +0100

    exo 2 on fire

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -21,15 +21,25 @@ class RestaurantsController < ApplicationController
   end
 
   def update
+    @restaurant = Restaurant.find(params[:id])
+     if @restaurant.update(restaurants_params)
+      redirect_to @restaurant, notice: 'Restaurant was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.'
   end
 
   def edit
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def restaurants_params
-    params.require(:restaurant).permit(:name, :address, :category)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
